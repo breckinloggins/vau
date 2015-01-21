e38c3e90bcbf844d215b737e3173567707686ba0
Breckin Loggins <breckin@apportable.com>
2015-01-21 09:03:48 -0800

    parse vau expressions to lists, numbers, and symbols

--- vau.py
+++ vau.py
@@ -1,6 +1,49 @@
+# Initial lisp interpreter based heavily on http://norvig.com/lispy.html
+
 __author__ = 'bloggins'
 
+
 import sys
+
+Symbol = str
+List = list
+Number = (int, float)
+
+
+def parse(program):
+    """Read a vau representation from a string"""
+    return read_from_tokens(tokenize(program))
+
+
+def read_from_tokens(tokens):
+    """Read an expression from a sequence of tokens"""
+    if len(tokens) == 0:
+        raise SyntaxError("unexpected end of input while reading")
+
+    token = tokens.pop(0)
+    if '(' == token:
+        read_list = []
+        while tokens[0] != ')':
+            read_list.append(read_from_tokens(tokens))
+        tokens.pop(0)   # pops off the last ')' we read
+        return read_list
+    elif ')' == token:
+        raise SyntaxError("unexpected ')' while reading")
+    else:
+        return atom(token)
+
+
+def tokenize(chars):
+    """Convert a string of characters into a list of tokens"""
+    return chars.replace('(', ' ( ').replace(')', ' ) ').split()
+
+
+def atom(token):
+    """Numbers become Python numbers; every other token is a symbol"""
+    try:
+        return int(token)
+    except ValueError:
+        return Symbol(token)
 
 
 def main():
@@ -12,7 +55,11 @@ def main():
         if input_line == "#quit":
             break
 
-        print "INPUT: %s" % input_line
+        try:
+            vau_program = parse(input_line)
+            print "%s" % vau_program
+        except SyntaxError as e:
+            print "error: %s" % e
 
 if __name__ == "__main__":
     main()
