fc01ebb8449dac177a87c48b21a5688682449bf1
Breckin Loggins <breckin@apportable.com>
2015-01-21 20:03:52 -0800

    use our own lexer

--- vau.py
+++ vau.py
@@ -1,15 +1,122 @@
-from __future__ import unicode_literals
+# -*- coding: utf-8 -*-
 
 # Initial lisp interpreter based heavily on http://norvig.com/lispy.html
 __author__ = 'bloggins'
 
-import sys
+import re
 import __builtin__
+from pygments.lexer import RegexLexer
+from pygments.token import Comment, Text, String, Name, Operator, Number, Keyword, Punctuation
 from prompt_toolkit.contrib.completers import WordCompleter
+
+
+vau_builtins = {
+    '$platform-object',
+    '$defsyntax!',
+    '$unquote',
+    '$if',
+    '$def!',
+    '$set!',
+    '$vau',
+    '$fn',
+    'wrap',
+}
+
+
+class VauLexer(RegexLexer):
+    """
+    A vau lexer, parsing a stream and outputting the tokens
+    needed to highlight vau code.
+    """
+
+    name = 'vau'
+    aliases = ['Vau', 'vau-lang']
+    filenames = ['*.vau']
+    mimetypes = ['text/x-vau', 'application/x-vau']
+
+    keywords = [k for k in vau_builtins]
+    builtins = (
+    )
+
+    # valid names for identifiers
+    # well, names can only not consist fully of numbers
+    # but this should be good enough for now
+    valid_name = r'[\w!$%&*+,/:<=>?@^~|-]+'
+
+    tokens = {
+        'root': [
+            # the comments
+            # and going to the end of the line
+            # (r';.*$', Comment.Single),
+            # multi-line comment
+            # (r'#\|', Comment.Multiline, 'multiline-comment'),
+            # commented form (entire sexpr folliwng)
+            # (r'#;\s*\(', Comment, 'commented-form'),
+            # signifies that the program text that follows is written with the
+            # lexical and datum syntax described in r6rs
+            # (r'#!r6rs', Comment),
+
+            # whitespaces - usually not relevant
+            (r'\s+', Text),
+
+            # numbers
+            (r'-?\d+\.\d+', Number.Float),
+            (r'-?\d+', Number.Integer),
+            # support for uncommon kinds of numbers -
+            # have to figure out what the characters mean
+            # (r'(#e|#i|#b|#o|#d|#x)[\d.]+', Number),
+
+            # strings, symbols and characters
+            # (r'"(\\\\|\\"|[^"])*"', String),
+            # (r"'" + valid_name, String.Symbol),
+            # (r"#\\([()/'\"._!§$%& ?=+-]|[a-zA-Z0-9]+)", String.Char),
+
+            # constants
+            # (r'(#t|#f)', Name.Constant),
+
+            # special operators
+            # (r"('|#|`|,@|,|\.)", Operator),
+
+            # highlight the keywords
+            ('(%s)' % '|'.join(re.escape(entry) + ' ' for entry in keywords),
+             Keyword),
+
+            # first variable in a quoted string like
+            # '(this is syntactic sugar)
+            # (r"(?<='\()" + valid_name, Name.Variable),
+            # (r"(?<=#\()" + valid_name, Name.Variable),
+
+            # highlight the builtins
+            # ("(?<=\()(%s)" % '|'.join(re.escape(entry) + ' ' for entry in builtins),
+            # Name.Builtin),
+
+            # the remaining functions
+            # (r'(?<=\()' + valid_name, Name.Function),
+            # find the remaining variables
+            # (valid_name, Name.Variable),
+
+            # the famous parentheses!
+            (r'(\(|\))', Punctuation),
+            (r'(\[|\])', Punctuation),
+        ],
+        # 'multiline-comment': [
+        #     (r'#\|', Comment.Multiline, '#push'),
+        #     (r'\|#', Comment.Multiline, '#pop'),
+        #     (r'[^|#]+', Comment.Multiline),
+        #     (r'[|#]', Comment.Multiline),
+        # ],
+        # 'commented-form': [
+        #     (r'\(', Comment, '#push'),
+        #     (r'\)', Comment, '#pop'),
+        #     (r'[^()]+', Comment),
+        # ],
+    }
+
 
 Symbol = str
 List = list
 Number = (int, float)
+
 
 class Combiner(object):
     """A user-defined vau combination"""
@@ -125,8 +232,6 @@ syntax_forms = []
 
 def repl(prompt='vau> '):
     """The vau read-evau-print loop"""
-    from pygments.lexers.lisp import SchemeLexer
-
     from prompt_toolkit import CommandLineInterface, AbortAction
     from prompt_toolkit import Exit
     from prompt_toolkit.layout import Layout
@@ -134,7 +239,7 @@ def repl(prompt='vau> '):
 
     cli = CommandLineInterface(layout=Layout(
         before_input=DefaultPrompt(prompt),
-        lexer=SchemeLexer
+        lexer=VauLexer
     ))
 
     try:
